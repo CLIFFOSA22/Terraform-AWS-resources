4b8f5fd6049e42ec6d93102457a1204f8ee58b4d
Clifford Egharevba <111459154+CLIFFOSA22@users.noreply.github.com>
2024-05-16 18:54:23 -0700

    Update s3.tf

--- s3-bucket-create/s3.tf
+++ s3-bucket-create/s3.tf
@@ -13,7 +13,7 @@
 #provider block
 provider "aws" {
    region  = "us-east-2"
-  # profile = "landmark" I commented here because i want to use my default credentials.
+  # profile = "devops" I commented here because i want to use my default credentials.
   /*
   profile is a name that has your IAM role credentials that you created in aws that can connect yoour local computer to your aws account after you can installed aws in your local system and also run aws configure to allow you put in your accessKey and secretKey
   */
